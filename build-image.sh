b7628a5b75ad787d574c712b3658c1f3758e07dd
Daniela Meneses <daniela11290@gmail.com>
2016-04-01 08:58:15 -0400

    Modified default path to create the output data folder

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 sudo docker build -t train-me-sandbox . 
-sudo docker create --name official-sandbox-2 --privileged -v /home/dann2/train-me/sandbox/output-data:/tmp/files train-me-sandbox
+sudo docker create --name official-sandbox-2 --privileged -v /home/memi/sandbox/output-data:/tmp/files train-me-sandbox
 #apt-get install asciidoc
 git clone https://github.com/dmeneses/isolate.git
 cd isolate
