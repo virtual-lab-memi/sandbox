8e18e6db9042350f39995adbbc4bf06e9d7d3bf1
Daniela Meneses <daniela11290@gmail.com>
2016-04-03 21:01:11 -0400

    Modified paths and order of build script

--- build-image.sh
+++ build-image.sh
@@ -1,8 +1,10 @@
-sudo docker build -t train-me-sandbox . 
-sudo docker create --name official-sandbox-2 --privileged -v /home/memi/sandbox/output-data:/tmp/files train-me-sandbox
 #apt-get install asciidoc
 git clone https://github.com/dmeneses/isolate.git
 cd isolate
 make
 cp isolate ../read-only-files/
+cd /home/memi/sandbox
+sudo docker build -t train-me-sandbox . 
+sudo docker create --name official-sandbox-2 --privileged -v /home/memi/sandbox/output-data:/tmp/files train-me-sandbox
+
 
